96deceb587533ef66fac8a243e45cef5f6f51a88
josephgunawan97 <joseph.gunawan97@gmail.com>
2020-10-31 15:17:44 +0700

    update

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,19 +3,18 @@ const execa = require("execa");
 const fs = require("fs");
 (async () => {
   try {
-    await execa("git", ["checkout", "--orphan", "new-master"]);
+    await execa("git", ["checkout", "--orphan", "deploy"]);
     // eslint-disable-next-line no-console
     console.log("Building started...");
     await execa("npm", ["run", "build"]);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
-    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
-    console.log("Pushing to gh-pages...");
-    await execa("git", ["push", "origin", "HEAD:new-master", "--force"]);
-    await execa("rm", ["-r", folderName]);
+    await execa("git", ["--work-tree", folderName, "commit", "-m", "deploy pages"]);
+    console.log("Pushing to deploy pages...");
+    await execa("git", ["push", "origin", "HEAD:deploy", "--force"]);
     await execa("git", ["checkout", "-f", "master"]);
-    await execa("git", ["branch", "-D", "new-master"]);
+    await execa("git", ["branch", "-D", "deploy"]);
     console.log("Successfully deployed, check your settings");
   } catch (e) {
     // eslint-disable-next-line no-console
