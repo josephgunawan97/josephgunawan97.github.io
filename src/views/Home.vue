586df68035d8b9aec4342616f1e8ba11d3de07a2
josephgunawan97 <joseph.gunawan97@gmail.com>
2020-10-31 22:46:57 +0700

    add vuetify and router

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Page msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Page from '@/components/Page.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Page
+  }
+}
+</script>
